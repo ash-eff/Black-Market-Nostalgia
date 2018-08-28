3074bcb2e73bb83583a00da93486bb0918bb9929
Ash Fischer <ash.s.fischer@gmail.com>
2018-08-28 16:09:32 -0500

    FIX camera pan

--- BlackMarketNostalgia/Assets/Scripts/TouchInput.cs
+++ BlackMarketNostalgia/Assets/Scripts/TouchInput.cs
@@ -6,8 +6,6 @@ public class TouchInput : MonoBehaviour {
 
     public LayerMask neighborhoodMask;
 
-    float deltaX, deltaY;
-
     RaycastHit2D hit;
 
     PlayerGUI playerGUI;
@@ -17,30 +15,32 @@ public class TouchInput : MonoBehaviour {
     private void Start()
     {
         playerGUI = FindObjectOfType<PlayerGUI>();
+        
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
         // if there is a touch
-        if (Input.touchCount == 1)
+        if (Input.touchCount > 0)
         {
             touch = Input.GetTouch(0);
             touchPos = Camera.main.ScreenToWorldPoint(touch.position);
 
-            hit = Physics2D.Raycast(touchPos, Vector3.zero);
+            hit = Physics2D.Raycast(touchPos, Vector3.zero, neighborhoodMask);
 
-            switch (touch.phase)
+            if (hit && Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                case TouchPhase.Began:
-
-                    if (hit.collider && hit.collider.tag == "Neighborhood")
-                    {
-                        playerGUI.NeighborhoodInformation(hit.transform.gameObject);
-                    }
-
-                    break;
+                playerGUI.NeighborhoodInformation(hit.transform.gameObject);
             }
 
+            if(Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+
+                // TODO figure out why the z axis on the camera changes when moving with finger
+                Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+                transform.Translate(new Vector3(-touchDeltaPosition.x * .10f, -touchDeltaPosition.y * .10f, 0));
+            }
         }
     }
 }
